81ea031bf9d0828885cd95dc2ffb3ed321d61181
Aino Havu <ahavu@student.hive.fi>
2025-06-09 15:00:20 +0300

    starting setup 2

---
+++ incl/cub3d.h
@@ -0,0 +1,16 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft_plus.h"
+# include "libftprintf.h"
+# include <math.h>
+
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define ESC 53
+
+
+
+#endif
