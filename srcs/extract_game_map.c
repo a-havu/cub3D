9bc383b893a4b24ac2d39aafe63621eb5e74846e
Aino Havu <ahavu@student.hive.fi>
2025-06-17 14:50:34 +0300

    1706

--- srcs/extract_game_map.c
+++ srcs/extract_game_map.c
@@ -15,15 +15,14 @@ static void	get_map_array(t_game *game, int fd)
 		line = get_next_line(fd);
 		if (!line)
 			break ;
-		// if (ft_strchr(line, '\n')) //laitoin takas
-		// 	*ft_strchr(line, '\n') = '\0'; //taa kans
+		// if (ft_strchr(line, '\n'))
+		// 	*ft_strchr(line, '\n') = '\0';
 		if (ft_strlen(line) > game->map->width)
 			game->map->width = ft_strlen(line);
 		game->map->array[i] = ft_strdup(line);
 		i++;
 	}
 	game->map->array[i] = NULL;
-	i = 0;
 }
 
 //worry later: Swen & Bug in same room
@@ -43,7 +42,7 @@ static void	flood_fill(t_game *game, int y, int x)
 	}
 	if (game->map_cpy[y + 1][x] != '1' && game->map_cpy[y + 1][x] != ' ')
 	{
-		if (y == 22)// game->map->height)
+		if (y == game->map->height)
 		{
 			// ft_printf("y: %i, x: %i, height: %i\n", y, x, game->map->height);
 			// ft_printf("error: %c\n", game->map_cpy[y + 1][x]);
@@ -76,23 +75,22 @@ static void	flood_fill(t_game *game, int y, int x)
 static void	copy_map(t_game *game)
 {
 	int	y;
+	int	i;
 
+	i = 0;
 	y = game->map->last_id + 2;
-	game->map_cpy = ft_calloc((game->map->height), sizeof(char *));
-	ft_printf("height: %d\n", y);
+	game->map_cpy = ft_calloc((game->map->height + 1), sizeof(char *));
 	if (!game->map_cpy)
 		ft_error(1, game);
 	while (game->map->array[y])
 	{
-		ft_printf("%s", game->map->array[y]);
-		game->map_cpy[y] = ft_strdup(game->map->array[y]);
-		if (!game->map_cpy[y])
+		game->map_cpy[i] = ft_strdup(game->map->array[y]);
+		if (!game->map_cpy[i])
 			ft_error(5, game);
 		y++;
+		i++;
 	}
-	ft_printf("helo\n");
-	game->map_cpy[y] = NULL;
-	ft_printf("%s", game->map_cpy[0]);
+	game->map_cpy[i] = NULL;
 }
 
 void	extract_game_map(char *arg, t_game *game)
@@ -119,7 +117,8 @@ void	extract_game_map(char *arg, t_game *game)
 		x = 0;
 		while (game->map_cpy[y][x])
 		{
-			if (game->map_cpy[y][x] != ' ' && game->map_cpy[y][x] != '1')
+			ft_printf("y: %d x: %d game->map_cpy[y][x]: %c\n", y, x, game->map_cpy[y][x]);
+			if (game->map_cpy[y][x] != ' ' && game->map_cpy[y][x] != '1' && game->map_cpy[y][x] != '\n')
 			{
 				ft_printf("Sending: %c from y: %i, x: %i\n", game->map_cpy[y][x], y, x);
 				flood_fill(game, y, x);
